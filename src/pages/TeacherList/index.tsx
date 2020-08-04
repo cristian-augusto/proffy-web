42195edbbfe99a4e4322a3378f751e88d89d383e
Cristian Augusto <cristianaugusto2013@gmail.com>
2020-08-04 12:04:06 -0300

    Finalizacao TeacherList

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
+
 
 function TeacherList() {
     return (
@@ -30,34 +31,9 @@ function TeacherList() {
             </PageHeader>
 
             <main>
-                <article className="teacher-item">
-                    <header>
-                        <img src="https://api.adorable.io/avatars/285/abott@adorable.png" alt="Cristian" />
-                        <div>
-                            <strong>Cristian Augusto</strong>
-                            <span>Química</span>
-                        </div>
-                    </header>
-
-                    <p>
-                        Entusiasta das melhores tecnologias de química avançada.
-                    <br /><br />
-                    Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-                    </p>
-
-                    <footer>
-                        <p>
-                            Preço/hora
-                            <strong>R$ 80,00</strong>
-                        </p>
-                        <button type="button">
-                            <img src={whatsappIcon} alt="Whatsapp" />
-                            Entrar em contato
-                        </button>
-                    </footer>
-
-
-                </article>
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
             </main>
         </div>
     )
