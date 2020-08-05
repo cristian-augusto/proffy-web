bf1ca7921e614ff5a5d49cfd60908911cc6c80b2
Cristian Augusto <cristianaugusto2013@gmail.com>
2020-08-05 12:28:46 -0300

    Component Input and page TeacherForm

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -5,6 +5,7 @@ import TeacherItem from '../../components/TeacherItem';
 
 
 import './styles.css';
+import Input from '../../components/Input';
 
 
 function TeacherList() {
@@ -12,21 +13,11 @@ function TeacherList() {
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject" />
-                    </div>
+                    <Input name="subject" label="Matéria" />
 
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da semana</label>
-                        <input type="text" id="week_day" />
-                    </div>
+                    <Input name="week_day" label="Dia da semana" />
 
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time" />
-                    </div>
-
+                    <Input type="time" name="time" label="Hora" />
                 </form>
             </PageHeader>
 
